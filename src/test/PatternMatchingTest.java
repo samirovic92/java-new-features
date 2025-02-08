a25c9b3a01c33c5ef90817fbb7cdc8dcbe67c985
selidrissi <s.elidrissi1992@gmail.com>
2025-02-08 11:38:52 +0100

    Pattern Matching for switch

--- src/test/PatternMatchingTest.java
+++ src/test/PatternMatchingTest.java
@@ -6,16 +6,30 @@ public class PatternMatchingTest {
     public static void main(String[] args) {
         // Pattern Matching of instance of
 
-        Object exception = new IllegalArgumentException("Hi, it's a bug!!!");
+        displayExceptionMessage(new IllegalArgumentException("Hi, it's a bug!!!"));
 
+        System.out.println(formatterWithSwitchPattern(null));
+        System.out.println(formatterWithSwitchPattern(1L));
+        System.out.println(formatterWithSwitchPattern("Hello world"));
+    }
+
+    public static void displayExceptionMessage(Exception exception) {
         if (exception instanceof IllegalFormatException ex) {
             System.out.println("The exception is IllegalFormatException, the message is " + ex.getMessage());
         } else if (exception instanceof IllegalArgumentException ex) {
             System.out.println("The exception is IllegalArgumentException, the message is : " + ex.getMessage());
         }
+    }
 
+    public static String formatterWithSwitchPattern(Object o ) {
+        return switch (o) {
+            case null -> "is Null";
+            case Integer i -> String.format("Integer %d", i);
+            case Long l -> String.format("Integer %d", l);
+            case Double d -> String.format("Double %f", d);
+            case String s -> String.format("String %s", s);
+            default -> o.toString();
+        };
     }
 }
 
-record Person(String name, int age) {
-}
